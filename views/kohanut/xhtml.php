3a312a684bc0bf84df0fceb3f4f9c5bb17643c88
Martin at 315W <Martin.Andrews@gmail.com>
2010-08-14 23:21:34 -0400

    Should use Kohana:DEVELOPMENT constant

--- views/kohanut/xhtml.php
+++ views/kohanut/xhtml.php
@@ -33,6 +33,6 @@
 
 <?php echo $layoutcode ?>
 <!-- End Page Layout Code -->
-<?php if (Kohana::$environment == 'development') echo View::factory('profiler/stats') ?>
+<?php if (Kohana::$environment == Kohana::DEVELOPMENT) echo View::factory('profiler/stats') ?>
 </body>
 </html>
